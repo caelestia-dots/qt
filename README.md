ee7f10b456d78f528ede2a1998f83c64ff1bc371
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-29 16:31:38 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# caelestia-qt
+
+QT5 and QT6 colour schemes for my caelestia dotfiles.
+
+This uses the [Darkly](https://github.com/Bali10050/Darkly) theme.
+
+## Installation
+
+Install [`caelestia-scripts`](https://github.com/caelestia-dots/scripts.git),
+then run `caelestia install qt`.
+
+## Usage
+
+If using `systemd`, the service will be installed and enabled automatically.
+Otherwise, autostart `$XDG_DATA_HOME/caelestia/qt/monitor/inotifywait.fish`.
---
+++ monitor/update.fish
@@ -0,0 +1,18 @@
+#!/bin/fish
+
+set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME || set -l config $HOME/.config
+set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.local/state
+set -l scheme_path $state/caelestia/scheme/current.txt
+set -l schemes (dirname (status filename))/../schemes
+
+# Update theme colours
+cp $schemes/template.conf $schemes/current.conf
+cat $scheme_path | while read line
+    set -l colour (string split ' ' $line)
+    sed -i "s/\$$colour[1]/#$colour[2]/g" $schemes/current.conf
+end
+test "$(cat $state/caelestia/scheme/current-mode.txt)" = light && set -l def 000000 || set -l def ffffff
+sed -i "s/\$default/#$def/g" $schemes/current.conf
+
+cp $schemes/current.conf $config/qt5ct/colors/caelestia.conf
+cp $schemes/current.conf $config/qt6ct/colors/caelestia.conf
