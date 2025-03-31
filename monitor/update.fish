48ec1476ffbbc61d721ea32a95e75e416b49043d
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-31 16:50:35 +1100

    ensure dir exists before copy

--- monitor/update.fish
+++ monitor/update.fish
@@ -14,5 +14,7 @@ end
 test "$(cat $state/caelestia/scheme/current-mode.txt)" = light && set -l def 000000 || set -l def ffffff
 sed -i "s/\$default/#$def/g" $schemes/current.conf
 
+mkdir -p $config/qt5ct/colors
+mkdir -p $config/qt6ct/colors
 cp $schemes/current.conf $config/qt5ct/colors/caelestia.conf
 cp $schemes/current.conf $config/qt6ct/colors/caelestia.conf
